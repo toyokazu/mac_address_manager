1f2074bcc098a3f0fc287a83442d25fb501eb761
Toyokazu Akiyama <toyokazu@gmail.com>
2010-04-09 19:39:16 +0900

    add register_dns and selectable flags to Location

--- db/migrate/20100320132145_create_locations.rb
+++ db/migrate/20100320132145_create_locations.rb
@@ -5,6 +5,8 @@ class CreateLocations < ActiveRecord::Migration
       t.string :hostname
       t.string :ipv4_addr
       t.string :ipv6_addr
+      t.boolean :register_dns, :default => false
+      t.boolean :selectable, :default => false
 
       t.timestamps
     end
