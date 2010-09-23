7d888eeb9364fa8725be3afd3186a21d630a7106
Toyokazu Akiyama <toyokazu@gmail.com>
2010-09-23 11:27:52 +0900

    change to use $stdin cse*_addrs.rb

--- script/csenic_addrs.rb
+++ script/csenic_addrs.rb
@@ -1,19 +1,9 @@
 #!/usr/bin/env ruby
 
-if ARGV.size < 1
-  puts "usage: csenic_addrs.rb csenic_converted_filename"
-  exit 1
-end
-
-csenic_addrs = nil
-open(ARGV[0] || "csenic.txt", "rb") do |csenic|
-  csenic_addrs = csenic.readlines
-end
-
 # data from csenic
 #   mac: mac_addr
-csenic_addrs.each do |csenic|
-  csenic.match(/^\s*mac:\s*(([\da-z]{2}:){5}[\da-z]{2})\s*/)
+while (str = $stdin.gets) do
+  str.match(/^\s*mac:\s*(([\da-z]{2}:){5}[\da-z]{2})\s*/)
   puts $1
 end
   
